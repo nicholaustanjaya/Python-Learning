5e1d93c0eb4cd418e4850416f8d25ccba5ed6272
-NkT- <nicholaustanjaya@gmail.com>
2025-06-07 23:11:03 +0900

    Totally change the content to conditional

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-num = float(input("Enter a number: "))
-if num > 0:
-    print("Positive")
-elif num == 0:
-    print("Zero")
-else:
-    print("Negative")
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+def print_prime(is_prime_flag,number):
+    if is_prime_flag:
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
+
+# number = int(input("Put some number = "))
+for i in range(2,51):
+    result = is_prime(i)
+    print_prime(result,i)
+
